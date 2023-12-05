2672ee274ef88a2dc829e213163e45e3fc9b3d9f
Oleg Zadorozhnyi <ipfound@gmail.com>
2023-12-05 18:02:11 +0200

    adds basic tests for a book.

--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -2,18 +2,74 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Book;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class BookReservationTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/books', [
+            'title' => 'Cool book title',
+            'author' => 'Victor',
+        ]);
+
+        $response->assertOk();
+
+        $this->assertCount(1, Book::all());
+    }
+
+    /** @test */
+    public function a_title_is_required()
+    {
+        // тут ми дозволяємо $this->withoutExceptionHandling();
+        // щоб вбуд.обробник викинув exception. результат його обробки запише в сесію значення title
+
+        $response = $this->post('/books', [
+            'title' => '',
+            'author' => 'Victor',
+        ]);
+
+        $response->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_author_is_required()
+    {
+        $response = $this->post('/books', [
+            'title' => 'Cool Book Title',
+            'author' => '',
+        ]);
+
+        $response->assertSessionHasErrors('author');
+    }
+
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/books', [
+            'title' => 'Cool Title',
+            'author' => 'Victor',
+        ]);
+
+        $book = Book::first();
+
+        $response = $this->patch('/books/'.$book->id, [
+            'title' => 'New Book Title',
+            'author' => 'New Author',
+        ]);
+
+        $book->refresh();
+
+        $this->assertEquals('New Book Title', $book->title);
+        $this->assertEquals('New Author', $book->author);
     }
 }
